b1c1326305584ba349f648984a961e9908fd90a1
vcs200831 <V.Chandra Shekhar>
2022-07-15 14:54:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# To take multiple user inputs
+# x, y, z = input("Enter the numbers: ").split()
+# print(x)
+# print(y)
+# print(z)
+
+# To satisfy all given conditions
+# subscriber = 2000
+# likes = 4000
+# comments = 100
+#
+# conditions = [subscriber > 500, likes > 500, comments > 50]
+#
+# if all(conditions):
+#     print("You are Popular")
+
+# To satisfy any given condition
+# sub = 1000
+# likes = 500
+# comments = 100
+# cond = [sub > 2000, likes > 600, comments > 50]
+#
+# if any(cond):
+#     print("You are Verified Singer")
+
+# To swap two values
+# a = 100
+# b = 200
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+
+# To find the unique values
+# a = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6]
+# print(a)
+# a = list(set(a))
+# print(a)
+
+
+# To find the most repeated element in the list
+# a = [5, 6, 7, 8, 9, 10, 5, 6, 7, 8, 9, 10,  5, 6, 7, 8, 9, 10,7,7]
+# print(a)
+# most_repeated = max(set(a), key=a.count)
+# print(most_repeated)
+
+
+# even_squares = [i**2 for i in range(20) if i % 2 == 0]
+# print(even_squares)
+
+
+# def sum(*b):
+#     result = 0
+#     for i in b:
+#         result = result + i
+#     return result
+#
+#
+# res = sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(res)
+
+# reverse the string
+# name = "Shekhar"[::-1]
+# print(name)
+
+# palindrome
+# name = "tattarrattat"
+# print(name == name[::-1])
+
+
+
